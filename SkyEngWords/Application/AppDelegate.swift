b26b016711ec4215fa4a1faf94714fc0312e0977
Alexandr <tsfadeev@yandex.ru>
2020-03-04 20:11:36 +0300

    Initial Commit

---
+++ SkyEngWords/Application/AppDelegate.swift
@@ -0,0 +1,12 @@
+//
+//  Copyright © 2020 Alexandr. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+}
---
+++ SkyEngWords/Resources/Colors/Colors.swift
@@ -0,0 +1,6 @@
+//
+//  Copyright © 2020 Alexandr. All rights reserved.
+//
+
+enum Colors {
+}
